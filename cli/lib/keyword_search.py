c182c864c2c24b3a3a0513861c887a176af3efb2
Vinzette <apoorva.kr03@gmail.com>
2026-02-07 16:33:52 +0000

    lowercase compatibility

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,10 +1,12 @@
 from lib.search_utils import load_movies
 
+
+
 def search_command(query, n_results):
     movies = load_movies()
     res=[]
     for movie in movies:
-        if query in movie['title']:
+        if query.lower() in movie["title"].lower():
             res.append(movie)
         if len(res)==n_results:
             break
