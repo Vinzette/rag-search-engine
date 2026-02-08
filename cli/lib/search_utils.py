9e9d088aeb2053eb9c41cc7734b0c5db57610d65
Vinzette <apoorva.kr03@gmail.com>
2026-02-08 07:41:43 +0000

    inverted index along with get, save, build func

--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -5,6 +5,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[2]
 DATA_PATH = PROJECT_ROOT/'data'
 MOVIES_PATH = DATA_PATH/'movies.json'
 STOPWORDS_PATH = DATA_PATH/'stopwords.txt'
+CACHE_PATH = PROJECT_ROOT/'cache'
 
 def load_movies() -> list[dict]:
     with open(MOVIES_PATH, "r") as f:
@@ -15,4 +16,6 @@ def load_stopwords():
     with open(STOPWORDS_PATH, "r") as f:
         data = f.read().splitlines()
     return data
+
+
 
